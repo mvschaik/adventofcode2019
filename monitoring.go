e96d0e939d57639fa6ee382fea95527d568889c5
Maarten van Schaik <maartenvs@google.com>
2019-12-10 12:12:32 +0100

    Completed day 10

--- monitoring.go
+++ monitoring.go
@@ -3,54 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
+	"sort"
+	"math"
 	"bufio"
 	"os"
 )
 
 func main() {
 	input := []string{
-		"......#.#.",
-		"#..#.#....",
-		"..#######.",
-		".#.#.###..",
-		".#..#.....",
-		"..#....#.#",
-		"#..#....#.",
-		".##.#..###",
-		"##...#..#.",
-		".#....####",
-	}
-	fmt.Println(bestLoc(input))
-
-	input = []string{
-		"#.#...#.#.",
-		".###....#.",
-		".#....#...",
-		"##.#.#.#.#",
-		"....#.#.#.",
-		".##..###.#",
-		"..#...##..",
-		"..##....##",
-		"......#...",
-		".####.###.",
-	}
-	fmt.Println(bestLoc(input))
-
-	input = []string{
-		".#..#..###",
-		"####.###.#",
-		"....###.#.",
-		"..###.##.#",
-		"##.##.#.#.",
-		"....###..#",
-		"..#.#..#.#",
-		"#..#.#.###",
-		".##...##.#",
-		".....#.#..",
-	}
-	fmt.Println(bestLoc(input))
-
-	input = []string{
 		".#..##.###...#######",
 		"##.############..##.",
 		".#.######.########.#",
@@ -72,10 +32,24 @@ func main() {
 		"#.#.#.#####.####.###",
 		"###.##.####.##.#..##",
 	}
-	fmt.Println(bestLoc(input))
+	best, _ := bestLoc(input)
+	field := parseInput(input)
+	fmt.Println(zap(field, best, 1))
+	fmt.Println(zap(field, best, 2))
+	fmt.Println(zap(field, best, 3))
+	fmt.Println(zap(field, best, 10))
+	fmt.Println(zap(field, best, 20))
+	fmt.Println(zap(field, best, 50))
+	fmt.Println(zap(field, best, 100))
+	fmt.Println(zap(field, best, 199))
+	fmt.Println(zap(field, best, 200))
+	fmt.Println(zap(field, best, 201))
+	fmt.Println(zap(field, best, 299))
 
 	input = readFile("monitoring.txt")
-	fmt.Println(bestLoc(input))
+	best2, _ := bestLoc(input)
+	solution := zap(parseInput(input), best2, 200)
+	fmt.Println("==> ", solution.x * 100 + solution.y)
 }
 
 func bestLoc(input []string) (loc, int) {
@@ -120,6 +94,55 @@ func bestLoc(input []string) (loc, int) {
 		}
 	}
 	return best, maxStars
+}
+
+func zap(field map[loc]bool, origin loc, idx int) loc {
+	allRcs := make(map[loc]bool)
+	for l, _ := range field {
+		if l == origin {
+			continue
+		}
+		x, y := rc(origin, l)
+		allRcs[loc{x, y}] = true
+	}
+	all := make([]loc, 0, len(allRcs))
+	for l := range allRcs {
+		all = append(all, l)
+	}
+
+	sort.Slice(all, func(i, j int) bool {
+		fst := math.Atan2(float64(-all[i].x) - .0001, float64(all[i].y))
+		snd := math.Atan2(float64(-all[j].x) - .0001, float64(all[j].y))
+		return fst < snd
+	})
+
+	zapped := make(map[loc]bool)
+	cnt := 0
+	for x := 0; x < 100; x++ {
+		for _, rc := range all {
+			for i := 1 ;; i++ {
+				pos := loc{origin.x + i * rc.x, origin.y + i * rc.y}
+				if pos.x < 0 || pos.x > 25 {
+					break
+				}
+				if pos.y < 0 || pos.y > 25 {
+					break
+				}
+				if zapped[pos] {
+					continue
+				}
+				if field[pos] {
+					zapped[pos] = true
+					cnt++
+					if cnt == idx {
+						return pos
+					}
+					break
+				}
+			}
+		}
+	}
+	return loc{-1,-1}
 }
 
 func gcd(a, b int) int {
