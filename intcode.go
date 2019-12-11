8d61d658618a5621ede7be893c66e44853da1a55
Maarten van Schaik <maartenvs@google.com>
2019-12-11 20:31:12 +0100

    Finished first part of day 11.

--- intcode.go
+++ intcode.go
@@ -10,7 +10,7 @@ import (
 	"io/ioutil"
 )
 
-func run(prog []int/*, in chan int, out chan int*/) int {
+func run(prog []int, in chan int, out chan int) int {
 	mem := make(map[int]int)
 	for i, v := range(prog) {
 		mem[i] = v
@@ -69,19 +69,19 @@ func run(prog []int/*, in chan int, out chan int*/) int {
 			pc += 4
 		case 3: // input
 			log.Print("input", mem[pc], mem[pc+1])
-			fmt.Print("Input: ")
+			//fmt.Print("Input: ")
 			var i int
-			_, err := fmt.Scanf("%d", &i)
-			if err != nil {
-				log.Fatal(err)
-			}
-			// i = <-in
+			//_, err := fmt.Scanf("%d", &i)
+			//if err != nil {
+		//		log.Fatal(err)
+			//}
+			i = <-in
 			set(1, i)
 			pc += 2
 		case 4: // output
 			log.Print("output", mem[pc], mem[pc+1])
-			fmt.Println("Output: ", get(1))
-			//out <- get(1)
+			// fmt.Println("Output: ", get(1))
+			out <- get(1)
 			pc += 2
 		case 5: // jump if true
 			log.Print("jump-if-true", mem[pc], mem[pc+1], mem[pc+2])
@@ -119,7 +119,7 @@ func run(prog []int/*, in chan int, out chan int*/) int {
 			pc += 2
 		case 99:
 			log.Print("exit", mem[pc])
-			//close(out)
+			close(out)
 			return mem[0]
 		default:
 			fmt.Println("PC: ", pc)
@@ -129,9 +129,128 @@ func run(prog []int/*, in chan int, out chan int*/) int {
 	return -1
 }
 
+type loc struct {
+	x, y int
+}
+
+var (
+	up = loc{0, -1}
+	down = loc{0, 1}
+	left = loc{-1, 0}
+	right = loc{1, 0}
+)
+
+func (l loc) move(dir loc) loc {
+	return loc{l.x + dir.x, l.y + dir.y}
+}
+
+func turnLeft(d loc) loc {
+	switch d {
+	case up:
+		return left
+	case left:
+		return down
+	case down:
+		return right
+	case right:
+		return up
+	}
+	return d
+}
+
+func turnRight(d loc) loc {
+	switch d {
+	case up:
+		return right
+	case right:
+		return down
+	case down:
+		return left
+	case left:
+		return up
+	}
+	return d
+}
+
 func main() {
-	prog := parseProgram(readFile("boost.txt"))
-	run(prog)
+	prog := parseProgram(readFile("paint.txt"))
+
+	field := make(map[loc]int)
+	in := make(chan int, 10)
+	out := make(chan int, 10)
+	pos := loc{0, 0}
+	dir := up
+	go run(prog, in, out)
+	in <- 0
+	for {
+		//fmt.Printf("@ %v %v\n", pos, dir)
+		//printField(field, pos, dir)
+		//fmt.Println()
+		// fmt.Scanln()
+
+		color, ok := <-out
+		if !ok {
+			fmt.Println("Done!")
+			break
+		}
+		d, ok := <-out
+		if !ok {
+			break
+		}
+
+		//fmt.Printf("(%v, %v)\n", color, d)
+
+		field[pos] = color
+		if d == 0 {
+			dir = turnLeft(dir)
+		} else {
+			dir = turnRight(dir)
+		}
+		pos = pos.move(dir)
+		in <- field[pos]
+	}
+	printField(field, pos, dir)
+	fmt.Println("Count: ", len(field))
+}
+
+func printField(field map[loc]int, pos, dir loc) {
+	var minx, miny, maxx, maxy int
+	for l, _ := range field {
+		if l.x < minx {
+			minx = l.x
+		}
+		if l.x > maxx {
+			maxx = l.x
+		}
+		if l.y < miny {
+			miny = l.y
+		}
+		if l.y > maxy {
+			maxy = l.y
+		}
+	}
+	for y := miny - 2; y <= maxy + 2; y++ {
+		for x := minx - 2; x <= maxx + 2; x++ {
+			l := loc{x, y}
+			if pos == l {
+				switch dir {
+				case up:
+					fmt.Print("^")
+				case down:
+					fmt.Print("v")
+				case left:
+					fmt.Print("<")
+				case right:
+					fmt.Print(">")
+				}
+			} else if field[l] == 1 {
+				fmt.Print("#")
+			} else {
+				fmt.Print(".")
+			}
+		}
+		fmt.Println()
+	}
 }
 
 func parseProgram(s string) []int {
